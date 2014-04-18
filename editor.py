188357402e51f6ea77f2171735ad345afd56cde4
Sam Hatfield <samuel.e.hatfield@gmail.com>
2014-04-18 00:05:03 -0500

    My test suite for editor is coming along. I used the Python Debugger, which was pretty cool. Hope to do more tomorrow.

--- editor.py
+++ editor.py
@@ -39,41 +39,85 @@ class AutoDiffBuffer:
 	def __init__(self, text_buffer = None):
 
 		self.text_buffer = text_buffer
-		self.ndiffer = difflib.Differ.ndiff()
-		self.restorer = difflib.Differ.restore()
+		self.ndiffer = difflib.ndiff
+		self.restorer = difflib.restore
 		self.save_buffer, self.redo_buffer = [], []
 
-	def get_text(self):
-		text_start = text_buffer.get_start_iter()
-		text_end = text_buffer.get_end_iter()
-		return self.text_buffer.get_text(text_start, text_end)
+	def get_edit(self):
+		text_start = self.text_buffer.get_start_iter()
+		text_end = self.text_buffer.get_end_iter()
+		return self.text_buffer.get_text(text_start, text_end, False)
 
 	def get_diff(self):
-		if isinstance(save_buffer[-1], str):
-			if isinstance(save_buffer[-2], str):
-				first_str = save_buffer.pop()
-				second_str = save_buffer.pop()
-				return self.ndiffer(first_str, second_str)
-		return
+		if self.save_buffer is not None:
+			newer_edit = self.save_buffer[-1]
+			older_edit = self.save_buffer[-2]
+			if isinstance(newer_edit, str) and isinstance(older_edit, str):
+				return self.ndiffer(newer_edit, older_edit)
+		return None
 
-	def save_text(self):
-		delta = get_diff())
-		if delta is None:
-			save_buffer.append(get_text())
+	def save_edit(self):
+		if len(self.save_buffer) < 2:
+			self.save_buffer.append(self.get_edit())
 		else:
-			save_buffer.append(delta)
-		redo_buffer.clear()
+			delta = self.get_diff()
+			if delta is None:
+				self.save_buffer.append(get_edit())
+			else:
+			# this keeps the two top elements text buffer strings
+				self.save_buffer[-2] = delta
+				self.save_buffer.append(self.get_edit())
+				self.redo_buffer.clear()
 		print("Text Saved!")
 
-	def restore_undo(self):
+	def restore_edit(self):
+		old_edit = self.save_buffer[-1]
+		if type(old_edit, str) is False:
+			old_edit = self.restore_diff(old_edit)
+		self.text_buffer.set_text(old_edit, len(old_edit))
+
+	def restore_diff(self, diff):
 		# the idea behind this one is to use the restore method
 		# to travel back through the deltas. This will also populate the
 		# redo list. If you notice above, the redo list gets cleared
 		# when the user is making a new save.
+		return ''.join(self.restorer(diff, 2))
+
+	def undo_edit(self):
+		self.redo_buffer.append(self.save_buffer.pop())
+		self.restore_edit()
+
+	def redo_edit(self):
+		self.save_buffer.append(self.redo_buffer.pop(0))
+		self.restore_edit()
 
 
 class FileDiffBuffer:
 	"""Brings the autosave/diff buffer into a file for longer storage"""
+	pass
 
 if __name__ == "__main__":
+	editList = ["I'm a sentence.",
+				"I'm the sentence.",
+				"I've written the sentence.",
+				"We've written a sentence.",
+				"We've written a story."]
 
+	textBox = Gtk.TextBuffer()
+	ad = AutoDiffBuffer(textBox)
+
+	for e in editList:
+		print("Current edit: " + e)
+		textBox.set_text(e, len(e))
+		ad.save_edit()
+
+
+	for i in [1, 2, 3]:
+		print("autoDiffer.save_buffer before:" + ad.save_buffer[i])
+		ad.undo_edit()
+		print("EDIT #" + i)
+		print("autoDiffer.get_text: " + ad.get_text())
+		print("autoDiffer.save_buffer after: " + ad.save_buffer[i])
+		print("autoDiffer.redo_buffer: " + ad.redo_buffer[i])
+
+	print("End of testing")
