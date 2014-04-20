7a58f76998f0721839dd88e2594e3704e50e1869
Sam Hatfield <samuel.e.hatfield@gmail.com>
2014-04-20 15:58:24 -0500

    The code runs, but it still does not perform. I started a test suite. The first one will simply mimic the init sequence in run.py

--- WordAgent.py
+++ WordAgent.py
@@ -10,56 +10,119 @@ class Handler:
 
 	def __init__(self, segment_buffer, differ, file_clerk):
 		self.segment_buffer = segment_buffer
+		self.differ = differ
 		self.file_clerk = file_clerk
 
 	def gtk_main_quit(self, *args):
 		Gtk.main_quit(*args)
 
 	def	on_segmentBuffer_modified_changed(self, widget):
-		self.differ.save_edit()
+		self.segment_buffer.save_edit()
 
 	def on_undoButton_clicked(self, widget):
-		self.differ.undo_edit()
+		self.segment_buffer.undo_edit()
 
 	def	on_redoButton_clicked(self, widget):
-		self.differ.redo_edit()
+		self.segment_buffer.redo_edit()
 
 	def on_saveButton_clicked(self, widget):
-		self.differ.save_edit()
+		self.segment_buffer.save_edit(self.differ)
 
 class Buffer(Gtk.TextBuffer):
+	"""Extends text buffer to include version lists"""
 	def __init__(self):
+		Gtk.TextBuffer.__init__(self)
 		self.diffs = []
 		self.redos = []
 		self.saves = []
 
-		self.undoing = False
+		self.string = """
+		Welcome to the Word Agent, the novel project management app!
+
+		Features currently in development:
+			Undo/Redo
+			File IO
+
+		Next Feature in the works:
+			Project management
+
+		Future Features:
+			Version Control System integration
+			Cloud Collaboration with WebRTC
+"""
+		self.text = self.set_text(self.string, len(self.string))
+		self.add_save()
 
 	def add_save(self):
 		self.saves.append(self.text)
 
-	def add_diff(self, differ, str1, str2):
-		self.diffs.append(differ.get_diff(str1, str2))
+	def pop_save(self, index=-1):
+		try:
+			return self.saves.pop(index)
+		except IndexError:
+			print("{}.saves list is empty".format(self))
 
-	def add_redo(self):
-		"""Pops element from saves, pushes it to redos"""
-		self.redos.append(self.saves.pop())
+	def add_diff(self, differ, str1, str2):
+		self.diffs.append(differ.generate_diff(str1, str2))
+
+	def pop_diff(self, index=-1):
+		try:
+			return self.diff.pop(index)
+		except IndexError:
+			print("{}.diff list is empty".format(self))
+
+	def enque_redo(self):
+		"""Pops element from saves, enques it to redos"""
+		self.redos.append(self.pop_save())
+
+	def deque_redo(self):
+		"""Pops redo from the front, as a queue"""
+		return self.redos.pop(0)
+
+	def save_edit(self, differ):
+
+		if len(self.saves) <= 2:
+			self.add_save()
+
+		else:
+			differ.set_matcher(older= self.text, newer= self.saves[-1])
+			ratio = differ.matcher_quick_ratio()
+
+			if ratio is not 1.0:
+				self.add_save()
+				self.redos.clear
 
 	def undo_edit(self):
-		self.undoing = True
-		self.add_redo()
+		self.enque_redo()
 		self.text = self.saves[-1]
 
+	def redo_edit(self):
+		self.add_save()
+		redo = self.deque_redo()
+		self.text = redo
 
-
-class Differ(difflib.Differ):
+class Differ:
 	"""Generates deltas using diff functions"""
+	def __init__(self, older='', newer=''):
+		self.ndiff = difflib.ndiff
+		self.restore = difflib.restore
+		self.set_matcher(older, newer)
 
-	def get_diff(self, older, newer):
+	def set_matcher(self, older='', newer=''):
+		"""Matcher helps with string comparisons in the Buffer"""
 		try:
-			return self.ndiff(newer_edit, older_edit)
+			self.matcher = difflib.SequenceMatcher(None, older, newer)
 		except TypeError:
-			print("Inputs must be strings.")
+			print("set_matcher inputs must be strings.")
+
+	def matcher_quick_ratio(self):
+		return self.matcher.quick_ratio()
+
+	def generate_diff(self, older='', newer=''):
+		try:
+			return self.ndiff(newer, older)
+		except TypeError:
+			print("generate_diff inputs must be strings.")
 
 	def restore_diff(self, diff, which):
 		return ''.join(self.restore(diff, which))
--- test_init.py
+++ test_init.py
@@ -5,18 +5,31 @@ from gi.repository import Gtk
 
 from WordAgent import *
 
+print("Begin initialization testing suite")
+
+print("Init Gtk.Builder as bob; adding UI file")
 bob = Gtk.Builder()
 bob.add_from_file("WordAgentApp.glade")
+print(bob)
 
+print("Init text buffer and text view objects")
 txt_bf = Buffer()
 txt_bx = bob.get_object("editorTextBox")
 txt_bx.set_buffer(txt_bf)
+print(txt_bf)
+print(txt_bx)
 
+print("Init main window")
 win = bob.get_object("mainWindow")
+print(win)
 
+print("Init differ and file_clerk")
 diff = Differ(txt_bf)
 clrk = FileClerk()
-bob.connect_signals(Handler(txt_bf, diff, clrk))
+print(diff)
+print(clrk)
 
-win.show_all()
-Gtk.main()
+print("Connecting signals from Handler")
+bob.connect_signals(Handler(txt_bf, diff, clrk))
+print(Handler)
+
