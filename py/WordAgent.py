323bd2681e86ede89c9f71fe4a5eb76035c2324b
Sam Hatfield <samuel.e.hatfield@gmail.com>
2014-05-06 23:05:47 -0500

    Working on the new version of the Segment class. I hope this will solve the multiple files/buffer/view issue that I've been running into. Plus, I need to revert the README on my local repo.

--- py/WordAgent.py
+++ py/WordAgent.py
@@ -2,64 +2,87 @@ from collections import deque
 from difflib import SequenceMatcher, ndiff, restore
 from gi.repository import Gtk, Gdk
 import io
-import tempfile
 
-# FIXME: there should be WordAgent.functions that encode much of what SignalHandler does now.
+welcome_message = """
+    Welcome to the Word Agent, the novel project management app!
 
-# DIALOG METHODS
+    Features currently in development:
+        Cut/Copy/Paste
+        New/Open files
+        Dialog boxes
 
-def about_app_dialog():
-    """Launch an About dialog window"""
-    dialog = Gtk.AboutDialog.new()
-    dialog.set_program_name("Word Agent")
-    dialog.set_authors(["Sam Hatfield", None])
-    dialog.set_version("0.1")
-    dialog.set_website("https://github.com/sehqlr/word-agent")
-    dialog.set_website_label("Fork us on GitHub!")
-    dialog.set_comments("A minimal text editor with a big future.")
-    response = dialog.run()
-    if response:
-        print("About closed")
-        dialog.destroy()
+    Next Feature in the works:
+        Project management
 
-def open_file_dialog(file_database):
-    """Launch a File/Open dialog window"""
-    dialog = Gtk.FileChooserDialog("Open a project", None,
-        Gtk.FileChooserAction.OPEN,
-        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-         Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-    response = dialog.run()
-    if response == Gtk.ResponseType.OK:
-        print("Open clicked")
-        file_database.add_file(dialog.get_filename())
-        dialog.destroy()
-    if response == Gtk.ResponseType.CANCEL:
-        print("Cancel clicked")
-        dialog.destroy()
+    Future Features:
+        Version Control System integration
+        Cloud Collaboration with WebRTC
+        "Post Production" formatting (Scribus integration?)
+"""
 
-def save_file_dialog(file_database, segment_buffer):
-    """Launch a File/Save dialog window"""
-    dialog = Gtk.FileChooserDialog("Save your project", None,
-        Gtk.FileChooserAction.SAVE,
-        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-         Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
-    response = dialog.run()
-    if response == Gtk.ResponseType.OK:
-        print("Save clicked")
-        file_database.add_file(dialog.get_filename())
-        file_database.save_curr(segment_buffer)
-        dialog.destroy()
-    if response == Gtk.ResponseType.CANCEL:
-        print("Cancel clicked")
-        dialog.destory()
+class DialogMaker:
+    """Provides static methods to build dialog windows"""
+
+    @staticmethod
+    def about_app_dialog():
+        """Launch an About dialog window"""
+        dialog = Gtk.AboutDialog.new()
+        dialog.set_program_name("Word Agent")
+        dialog.set_authors(["Sam Hatfield", None])
+        dialog.set_version("0.1")
+        dialog.set_website("https://github.com/sehqlr/word-agent")
+        dialog.set_website_label("Fork us on GitHub!")
+        dialog.set_comments("A minimal text editor with a big future.")
+        response = dialog.run()
+        if response:
+            print("About closed")
+            dialog.destroy()
+
+    @staticmethod
+    def open_file_dialog():
+        """Launch a File/Open dialog window"""
+        dialog = Gtk.FileChooserDialog("Open a project", None,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            project_name = dialog.get_filename()
+            project_file = open(project_name, "r+")
+            dialog.destroy()
+        if response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+            dialog.destroy()
+        return project_file
+
+    @staticmethod
+    def saveas_file_dialog(text):
+        """Launch a File/Save dialog window"""
+        dialog = Gtk.FileChooserDialog("Save your project", None,
+            Gtk.FileChooserAction.SAVE,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_SAVE, Gtk.ResponseType.OK))
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Save clicked")
+            project_name = dialog.get_filename()
+            project_file = open(project_name, "r+")
+            project_file.write(text)
+            dialog.destroy()
+        if response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+            dialog.destory()
+        return project_file
 
 
 class SignalHandler:
     """Handles user events and file IO"""
-    # FIXME: It would be better to have this class focus on error checking and logging.
-    def __init__(self, segment_buffer, file_database):
+    # TODO: Transistion this class to error handling and logging after v0.2. Each handler should have less than 10 lines of code, except for error checking.
+    def __init__(self, segment_buffer, project_file):
         self.bfr = segment_buffer
-        self.fdb = file_database
+        self.pf = project_file
+        self.file_is_saved_as = False
         self.msg = "HANDLER: "
 
         # adding custom signals here
@@ -67,7 +90,7 @@ class SignalHandler:
 
     def gtk_main_quit(self, *args):
         print(self.msg, "gtk_main_quit")
-        self.fdb.close_project_files()
+        self.pf.close()
         Gtk.main_quit(*args)
 
     def buffer_changed(self, widget):
@@ -77,21 +100,28 @@ class SignalHandler:
 
     def on_newButton_clicked(self, widget):
         print(self.msg, "on_newButton_clicked")
-        self.fdb.add_file("untitled")
+        self.pf.close()
+        self.pf = open("untitled.wa.txt", "w")
+        self.bfr = SegmentBuffer.new_from_file(self.pf, self.bfr.text_view)
+        self.file_is_saved_as = False
 
     def on_openButton_clicked(self, widget):
         print(self.msg, "on_openButton_clicked")
-        open_file_dialog(self.fdb)
+        self.pf = DialogMaker.open_file_dialog()
+        self.bfr = SegmentBuffer.new_from_file(self.pf, self.bfr.text_view)
+        self.file_is_saved_as = True
 
     def on_saveButton_clicked(self, widget):
         print(self.msg, "on_saveButton_clicked")
-        if self.fdb.curr_file is None:
-            save_file_dialog(self.fdb, self.bfr)
-        self.fdb.save_curr(self.bfr)
+        if self.file_is_saved_as is not True:
+            self.pf = DialogMaker.saveas_file_dialog(self.bfr.curr)
+        else:
+            self.pf.write(self.bfr.curr)
 
     def on_saveasButton_clicked(self, widget):
         print(self.msg, "on_saveButton_clicked")
-        save_file_dialog(self.fdb, self.bfr)
+        self.pf = DialogMaker.saveas_file_dialog(self.bfr.curr)
+        self.file_is_saved_as = True
 
     def on_undoButton_clicked(self, widget):
         print(self.msg, "on_undoButton_clicked")
@@ -105,36 +135,45 @@ class SignalHandler:
 
     def on_cutButton_clicked(self, widget):
         print(self.msg, "on_cutButton_clicked")
-        cut_to_clipboard(self.bfr)
+        self.bfr.cut_to_clipboard()
 
     def on_copyButton_clicked(self, widget):
         print(self.msg, "on_copyButton_clicked")
-        copy_to_clipboard()
+        self.bfr.copy_to_clipboard()
 
     def on_pasteButton_clicked(self, widget):
         print(self.msg, "on_pasteButton_clicked")
-        paste_from_clipboard()
+        self.bfr.paste_from_clipboard()
 
     def on_aboutButton_clicked(self, widget):
         print(self.msg, "on_aboutButton_clicked")
-        about_app_dialog()
+        DialogMaker.about_app_dialog()
 
-class SegmentBuffer(Gtk.TextBuffer):
-    """With autosave, undo/redo, cut/copy/paste, sequence matcher"""
-    def __init__(self, segment="Default text"):
-        Gtk.TextBuffer.__init__(self)
+class Segment:
+    """Organizes segments, including buffers, views, and files"""
+    def __init__(self):
+        self.buffer = Gtk.TextBuffer.new()
+        self.view = Gtk.TextView.new_with_buffer(self.buffer)
 
         # None is a sentinel value. The newline is for setting text.
-        self.edits = deque([None, segment + '\n'])
+        self.edits = deque([None, welcome_message + '\n'])
 
-        self.set_text(segment)
-        self.prev = segment
-        self.curr = segment
+        self.buffer.set_text(welcome_message)
+        self.prev = welcome_message
+        self.curr = welcome_message
 
         self.matcher = SequenceMatcher()
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
-        self.add_selection_clipboard(self.clipboard)
+        self.buffer.add_selection_clipboard(self.clipboard)
 
+    @staticmethod
+    def new_from_file():
+        text = ""
+        for line in project_file:
+            text += line
+        seg_buffer = SegmentBuffer(segment=text, text_view=text_view)
+        text_view.set_buffer(seg_buffer)
+        return seg_buffer
 
     # AUTOSAVE METHODS
     def text_comparison(self):
@@ -187,41 +226,44 @@ class SegmentBuffer(Gtk.TextBuffer):
     def paste_from_clipboard(self):
         self.paste_clipboard(self.clipboard, None, True)
 
-class FileDatabase:
-    """Handles file I/O and organizes segments and projects"""
-    def __init__(self):
-        self.file_suffix = ".wa.txt"
-        self.open_files = []
-        self._curr_file = None
-
-    @property
-    def curr_file(self):
-        return self._curr_file
-
-    @curr_file.setter
-    def curr_file(self, value):
-        self._curr_file = value
-
-    def add_file(self, name):
-        name = str(name) + self.file_suffix
-        addf = open(name, "w")
-        self.open_files.append(addf)
-        return addf
-
-    def fetch_file(self, index= -1):
-        self.curr_file = self.open_files[index]
-
-    # NEW/OPEN/SAVE METHODS
-
-    def save_curr(self, segment_buffer):
-        if self.curr_file is None:
-            newf = self.add_file("untitled")
-            self.curr_file = newf
-        self.curr_file.write(segment_buffer.curr)
-
-    def close_project_files(self):
-        for f in self.open_files:
-            try:
-                f.close()
-            except AttributeError:
-                print(f, "is not a file object.")
+# This class is part of the Project Management feature.
+# Work will begin again in v0.3
+#~
+#~ class FileDatabase:
+    #~ """Handles file I/O and organizes segments and projects"""
+    #~ def __init__(self):
+        #~ self.file_suffix = ".wa.txt"
+        #~ self.open_files = []
+        #~ self._curr_file = None
+#~
+    #~ @property
+    #~ def curr_file(self):
+        #~ return self._curr_file
+#~
+    #~ @curr_file.setter
+    #~ def curr_file(self, value):
+        #~ self._curr_file = value
+#~
+    #~ def add_file(self, name):
+        #~ name = str(name) + self.file_suffix
+        #~ addf = open(name, "w")
+        #~ self.open_files.append(addf)
+        #~ return addf
+#~
+    #~ def fetch_file(self, index= -1):
+        #~ self.curr_file = self.open_files[index]
+#~
+    #~ # NEW/OPEN/SAVE METHODS
+#~
+    #~ def save_curr(self, segment_buffer):
+        #~ if self.curr_file is None:
+            #~ newf = self.add_file("untitled")
+            #~ self.curr_file = newf
+        #~ self.curr_file.write(segment_buffer.curr)
+#~
+    #~ def close_project_files(self):
+        #~ for f in self.open_files:
+            #~ try:
+                #~ f.close()
+            #~ except AttributeError:
+                #~ print(f, "is not a file object.")
--- run.py
+++ run.py
@@ -7,42 +7,23 @@ from gi.repository import Gtk, Gdk
 
 import py.WordAgent as WA
 
-welcome_message = """
-    Welcome to the Word Agent, the novel project management app!
-
-    Features currently in development:
-        Cut/Copy/Paste
-        New/Open files
-        Dialog boxes
-
-    Next Feature in the works:
-        Project management
-
-    Future Features:
-        Version Control System integration
-        Cloud Collaboration with WebRTC
-        "Post Production" formatting (Scribus integration?)
-"""
-
 # Path Strings
 src_path = os.getcwd()
 main_glade_file = os.path.join(src_path + "/ui/MainApp.glade")
 testing_dir = src_path + "/testing"
 
-# Bob the Builder
+# Bob the Gtk.Builder
 bob = Gtk.Builder.new()
 bob.add_from_file(main_glade_file)
 
 # SegmentBuffer and associated TextView
-bfr = WA.SegmentBuffer(welcome_message)
 txt = bob.get_object("editTextView")
+bfr = WA.SegmentBuffer(text_view=txt)
 txt.set_buffer(bfr)
 
-fdb = WA.FileDatabase()
+pf = open("untitled.wa.txt", "w")
 
-os.chdir(testing_dir)
-
-bob.connect_signals(WA.SignalHandler(bfr, fdb))
+bob.connect_signals(WA.SignalHandler(bfr, pf))
 
 app = bob.get_object("topWindow")
 app.show()
